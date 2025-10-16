de3ba0b489594b158e52faeab83b8b47809d8074
Daniel <danielp4p1@gmail.com>
2025-10-16 12:37:48 -0500

    Commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -15,6 +15,7 @@ public class Proyecto01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Direccion de UTPL: Calle Paris S/N");
     }
     
 }
