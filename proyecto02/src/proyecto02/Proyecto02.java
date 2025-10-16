882ac69503f48de0db809d78c6c9c487177bacbd
Daniel <danielp4p1@gmail.com>
2025-10-16 12:40:33 -0500

    2

--- proyecto02/src/proyecto02/Proyecto02.java
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -15,6 +15,7 @@ public class Proyecto02 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Ingenieria en Ciencias de la Computación, tiene 9 ciclos");
     }
     
 }
